f37c63402df8cae7b287a2dcc67ad6aa3cd9d0f0
Nannie Ingersdotter <emelie.l.gustafsson@gmail.com>
2024-08-11 11:54:10 +0200

    Added Items and People tables

--- model/init_db.sql
+++ model/init_db.sql
@@ -2,17 +2,30 @@
 -- Drop Tables
 --
 
-SET foreign_key_checks = 0;
-DROP TABLE if exists students;
-SET foreign_key_checks = 1;
+-- SET foreign_key_checks = ;
+DROP TABLE if exists Items;
+DROP TABLE if exists People;
 
 --
 -- Create Tables
 --
 
-CREATE TABLE students(
-    id INT NOT NULL AUTO_INCREMENT, 
-    firstname VARCHAR(40) not null, 
-    lastname VARCHAR(40) not null, 
-    PRIMARY KEY (id)
-    );
+
+CREATE TABLE `Items`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `item` VARCHAR(255) NOT NULL,
+    `free` BOOLEAN NOT NULL,
+    `belongs_to` INT NOT NULL,
+    `borrowed_by` INT NOT NULL
+);
+CREATE TABLE `People`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE KEY,
+    `first_name` VARCHAR(255) NOT NULL,
+    `last_name` VARCHAR(255) NOT NULL,
+    `floor` INT NOT NULL,
+    `email` VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    `Items` ADD CONSTRAINT `items_belongs_to_foreign` FOREIGN KEY(`belongs_to`) REFERENCES `People`(`id`);
+ALTER TABLE
+    `Items` ADD CONSTRAINT `items_borrowed_by_foreign` FOREIGN KEY(`borrowed_by`) REFERENCES `People`(`id`);
